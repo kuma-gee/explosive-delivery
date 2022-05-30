cc8ff2b19b75843289682d4f378c6265cfaed1eb
myin <minyin142p@gmail.com>
2022-05-30 10:33:10 +0200

    add sprinting

--- src/prototyping/behaviour/PlayerMoveTop2D.gd
+++ src/prototyping/behaviour/PlayerMoveTop2D.gd
@@ -1,15 +1,21 @@
 class_name PlayerMoveTop2D extends Node
 
 export var speed := 500
+export var sprint_speed := 800
 
 export var input_path: NodePath
 onready var input := get_node(input_path)
 
 onready var body: KinematicBody2D = get_parent()
 
+onready var current_speed = speed
+
+func _process(_delta):
+	current_speed = sprint_speed if input.is_pressed("sprint") else speed
+
 func _physics_process(_delta):
 	var dir = _get_motion()
-	body.move_and_slide(dir * speed)
+	body.move_and_slide(dir * current_speed)
 
 
 func _get_motion() -> Vector2:
