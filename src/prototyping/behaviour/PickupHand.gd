dba83b5470b2c24677f48e733c0cbc28da58cc32
myin <minyin142p@gmail.com>
2022-05-30 10:59:09 +0200

    pickup facing object

--- src/prototyping/behaviour/PickupHand.gd
+++ src/prototyping/behaviour/PickupHand.gd
@@ -14,14 +14,29 @@ func _unhandled_input(_event):
 			_place_item()
 				
 func _pickup_item():
+	var closest_item = null
+	var closest_item_dot_scale = -1
 	for area in get_overlapping_areas():
 		if area is PickupItem:
-			holding_item = area.duplicate()
-			item = area.pickup_item.instance()
-			add_child(item)
-			item.position = Vector2.ZERO
-			area.queue_free()
-			
+			if closest_item == null:
+				closest_item = area
+			else:
+				var item_dir = (global_position + area.global_position).normalized()
+				var hand_dir = Vector2.RIGHT.rotated(global_rotation)
+				var dot_scale = item_dir.dot(hand_dir)
+				
+				if dot_scale > closest_item_dot_scale:
+					closest_item = area
+					closest_item_dot_scale = dot_scale
+
+	if closest_item:
+		holding_item = closest_item.duplicate()
+		item = holding_item.pickup_item.instance()
+		add_child(item)
+		item.position = Vector2.ZERO
+		closest_item.queue_free()
+
+
 func _place_item():
 	get_tree().current_scene.add_child(holding_item)
 	holding_item.global_position = global_position
