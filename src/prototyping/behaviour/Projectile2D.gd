73dae24670212eb2d887d97a8974c585dd4e76d6
myin <minyin142p@gmail.com>
2022-07-09 13:36:56 +0200

    always bounce with weight

--- src/prototyping/behaviour/Projectile2D.gd
+++ src/prototyping/behaviour/Projectile2D.gd
@@ -1,8 +1,7 @@
 class_name Projectile2D extends Node
 
-export var bounce = false
 export var deceleration := 500
-export var weight := 1
+export var weight := 1.0
 
 onready var body: KinematicBody2D = get_parent()
 
@@ -11,5 +10,6 @@ var velocity := Vector2.ZERO
 func _physics_process(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	var collision = body.move_and_collide(velocity)
-	if collision and bounce:
-		velocity = velocity.bounce(collision.normal)
+	if collision:
+		var normalized_weight = max(weight, 1)
+		velocity = velocity.bounce(collision.normal) / normalized_weight
