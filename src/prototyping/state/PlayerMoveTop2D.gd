3f24611ee60f1fcd2a4b6767c3d9fb2627919902
myin <minyin142p@gmail.com>
2022-07-09 11:16:14 +0200

    update movement with accel and decel

--- src/prototyping/state/PlayerMoveTop2D.gd
+++ src/prototyping/state/PlayerMoveTop2D.gd
@@ -1,21 +1,25 @@
 class_name PlayerMoveTop2D extends Node
 
-export var speed := 500
-export var sprint_speed := 800
+export var acceleration := 1000
+export var deceleration := 1000
+export var max_speed := 500
 
 export var input_path: NodePath
 onready var input := get_node(input_path)
 
 onready var body: KinematicBody2D = get_parent()
 
-onready var current_speed = speed
+var velocity = Vector2.ZERO
 
-func _process(_delta):
-	current_speed = sprint_speed if input.is_pressed("sprint") else speed
-
-func _physics_process(_delta):
-	var dir = _get_motion()
-	body.move_and_slide(dir * current_speed)
+func _physics_process(delta):
+	var direction = _get_motion()
+	var desired_velocity = direction * max_speed
+	
+	var is_moving = direction.length() > 0.01
+	var max_speed_change = acceleration if is_moving else deceleration
+	
+	velocity = velocity.move_toward(desired_velocity, max_speed_change * delta)
+	velocity = body.move_and_slide(velocity)
 
 
 func _get_motion() -> Vector2:
